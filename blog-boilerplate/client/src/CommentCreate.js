3472dc8f959c4b7dbaf797ddf362db103fe07d92
ser0710 <serroz2822@gmail.com>
2023-09-14 15:36:29 -0500

    finish first project

--- blog-boilerplate/client/src/CommentCreate.js
+++ blog-boilerplate/client/src/CommentCreate.js
@@ -5,7 +5,7 @@ const CommentCreate = ({ postID }) => {
     const [content, setContent] = useState('');
     const onSubmit = async (event) => {
         event.preventDefault();
-        await axios.post(`http://localhost:4001/posts/${postID}/comments`, { content });
+        await axios.post(`http://posts.com/posts/${postID}/comments`, { content });
         setContent('');
     }
     return (
