7fa713c9d2aeb59fa0e29936515fc0476b05fe1b
ser0710 <serroz2822@gmail.com>
2023-10-04 12:28:32 -0500

    npm organization

--- 3. ticketing/auth/src/routes/current-user.ts
+++ 3. ticketing/auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@ser0710_tic/common';
 
 const router = express.Router();
 
--- 3. ticketing/auth/src/routes/signin.ts
+++ 3. ticketing/auth/src/routes/signin.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest, BadRequestError } from '@ser0710_tic/common';
 import { Passowrd } from '../services/password';
 import  jwt  from 'jsonwebtoken';
 
